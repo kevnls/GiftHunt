b6c3aebce85630d5246a04eb5bcf7445bbe3b9de
kevnls <kevnls@hotmail.com>
2016-01-07 18:27:15 -0800

    Fixed the snow rotation

--- Assets/kevnls/Scripts/Snow.cs
+++ Assets/kevnls/Scripts/Snow.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class Snow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	   
+        //keep the attached transform faced one direction
+        this.transform.Rotate(0, 25, 0);
 	}
 }
